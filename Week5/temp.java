9fb1bba5a47829d82bd8bfbe1b2fb374643198f7
infiniteverma <vermaanant25@gmail.com>
2019-10-02 06:53:57 +0530

    add week5 solutions

---
+++ Week5/temp.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class q4{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner();
+		System.out.println("Enter number");
+		String num = sc.nextLine();
+
+		int size = num.length();
+		int i=size;
+		while(i>0){
+			i=
+		}
+	}
+}
